e113f8fbc69f870c37dbc84ab7baaa92f417caf4
alcasinoroyale <alexzdatny@gmail.com>
2019-12-13 23:48:26 -0500

    Update Heroes Component and Hero Details with Add and Delete Functions

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,10 +4,12 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
+  
   constructor(
     private messageService: MessageService,
     private http: HttpClient
@@ -17,14 +19,39 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
   private heroesUrl = 'api/heroes';
+  
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  }
+ getHeroes (): Observable<Hero[]> {
+  return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+}
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+    console.error(error); // log to console instead
+    this.log(`${operation} failed: ${error.message}`);
+    return of(result as T);
+  };
+}
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
   }
+
+  updateHero (hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
 }
